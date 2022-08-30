a6d39a8cc7fbfe73dbbf1d0656fd7e2f5885256e
sheriffkule <zeljko225@yahoo.com>
2022-08-30 15:27:16 +0200

    adding global colors, and editing navigation configuration

--- App.js
+++ App.js
@@ -3,10 +3,12 @@ import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import ManageExpense from './screens/ManageExpense';
 import RecentExpenses from './screens/RecentExpenses';
 import AllExpenses from './screens/AllExpenses';
+import {GlobalStyles} from './constants/styles';
 
 const Stack = createNativeStackNavigator();
 
@@ -14,9 +16,37 @@ const BottomTabs = createBottomTabNavigator();
 
 function ExpencesOverwiev() {
   return (
-    <BottomTabs.Navigator>
-      <BottomTabs.Screen name="Recent Expences" component={RecentExpenses} />
-      <BottomTabs.Screen name="All Expences" component={AllExpenses} />
+    <BottomTabs.Navigator
+      screenOptions={{
+        headerStyle: {backgroundColor: GlobalStyles.colors.primary500},
+        headerTintColor: GlobalStyles.colors.primary900,
+        tabBarStyle: {backgroundColor: GlobalStyles.colors.primary500},
+        tabBarActiveTintColor: GlobalStyles.colors.accent500,
+      }}>
+      <BottomTabs.Screen
+        name="RecentExpences"
+        component={RecentExpenses}
+        options={{
+          headerTitleAlign: 'center',
+          title: 'Recent Expences',
+          tabBarLabel: 'Recent',
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name="hourglass" size={size} color={color} />
+          ),
+        }}
+      />
+      <BottomTabs.Screen
+        name="All Expences"
+        component={AllExpenses}
+        options={{
+          headerTitleAlign: 'center',
+          title: 'All Expences',
+          tabBarLabel: 'All Expences',
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name="calendar" size={size} color={color} />
+          ),
+        }}
+      />
     </BottomTabs.Navigator>
   );
 }
@@ -27,7 +57,13 @@ export default function App() {
       <StatusBar barStyle={'light-content'} />
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="ExpencesOverwiev" component={ExpencesOverwiev} />
+          <Stack.Screen
+            name="Expences Overwiev"
+            component={ExpencesOverwiev}
+            options={{
+              headerShown: false,
+            }}
+          />
           <Stack.Screen name="Manage Expense" component={ManageExpense} />
         </Stack.Navigator>
       </NavigationContainer>
---
+++ constants/styles.js
@@ -0,0 +1,17 @@
+export const GlobalStyles = {
+  colors: {
+    primary50: '#e4d9fd',
+    primary100: '#c6affc',
+    primary200: '#a281f0',
+    primary400: '#5721d4',
+    primary500: '#20272F',
+    primary700: '#2d0689',
+    primary800: '#200364',
+    primary900: '#4EC5F1',
+    accent500: '#ffcc55',
+    error50: '#fcc4e4',
+    error500: '#9b095c',
+    gray500: '#39324a',
+    gray700: '#221c30',
+  },
+};
