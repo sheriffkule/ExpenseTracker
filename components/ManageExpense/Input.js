697758f0b984665c9f8aa6956d8b21704db7970c
sheriffkule <zeljko225@yahoo.com>
2022-09-03 15:05:42 +0200

    adding styling

--- components/ManageExpense/Input.js
+++ components/ManageExpense/Input.js
@@ -1,13 +1,43 @@
 import React from 'react';
-import {Text, TextInput, View} from 'react-native';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
 
-function Input({label, TextInputConfig}) {
+import {GlobalStyles} from '../../constants/styles';
+
+function Input({label, textInputConfig}) {
+  const inputStyles = [styles.input];
+  if (textInputConfig && textInputConfig.multiline) {
+    inputStyles.push(styles.inputMultiline);
+  }
+
   return (
-    <View>
-      <Text>{label}</Text>
-      <TextInput {...TextInputConfig} />
+    <View style={styles.inputContainer}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput style={inputStyles} {...textInputConfig} />
     </View>
   );
 }
 
 export default Input;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 10,
+  },
+  label: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary200,
+    marginBottom: 5,
+  },
+  input: {
+    backgroundColor: GlobalStyles.colors.primary200,
+    color: GlobalStyles.colors.primary500,
+    padding: 5,
+    borderRadius: 8,
+    fontSize: 18,
+  },
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: 'top',
+  },
+});
--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -86,6 +86,7 @@ const styles = StyleSheet.create({
   buttonStyle: {
     minWidth: 140,
     marginHorizontal: 10,
+    marginTop: 12,
   },
   deleteContainer: {
     marginTop: 20,
